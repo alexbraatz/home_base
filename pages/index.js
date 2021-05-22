dd0286ee842492390bbbc346c8521fc5bf83c92a
alexbraatz <alexjbraatz@gmail.com>
2021-05-22 14:53:36 -0500

    add profile photo and base components

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Alex's Home Site</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Image
+        src="/images/profile.jpg"
+        height={400}
+        width={600}
+        alt="Alex"
+      />
 
       <main>
         <h1 className="title">
