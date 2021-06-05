640d601f7b6864da94d04d9e732b117d7ebdd3f4
alexbraatz <alexjbraatz@gmail.com>
2021-06-04 20:33:05 -0500

    hello there

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -7,7 +7,7 @@ export default function FirstPost() {
     return (
         <Layout>
             <Head>
-                <title>First Post</title>
+                <title>Hello World</title>
             </Head>
             <h1>First Post</h1>
 
