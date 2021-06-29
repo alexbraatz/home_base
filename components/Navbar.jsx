952e865df8b9ad82cb79218ca6071b60b3ff8700
alexbraatz <alexjbraatz@gmail.com>
2021-06-29 12:49:19 -0500

    add desktop view

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -24,7 +24,7 @@ export const Navbar = () => {
 
 				<button className='inline-flex p-3 hover:bg-green-600 rounded text-white ml-auto hover:text-white outline-none'>
 					<svg
-						className='w-10 h-10'
+						className='w-9 h-9'
 						fill='none'
 						stroke='currentColor'
 						viewBox='0 0 24 24'
@@ -38,8 +38,29 @@ export const Navbar = () => {
 					</svg>
 				</button>
 
-				<div className='hidden w-full lg:inline-flix lg:flex-grow lg:w-auot'>
-					
+				<div className='hidden w-full lg:inline-flix lg:flex-grow lg:w-auto'>
+					<div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+						<Link href='/'>
+							<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+								Home
+							</a>
+						</Link>
+						<Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                Services
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                About us
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                Contact us
+              </a>
+            </Link>
+					</div>
 				</div>
 			</nav>	
 		</>
