2edb7fb1504fbb86c4bc27c71f22e3fedfa258ef
alexbraatz <alexjbraatz@gmail.com>
2021-06-29 12:40:57 -0500

    add hamburger menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo from '../public/images/myLogo.svg'
 export const Navbar = () => {
 	return (
 		<>
-			<nav className='flex items center flex-wrap bg-green-300 p-3 '>
+			<nav className='flex items center flex-wrap bg-red-400 p-3 '>
 				<Link href='/'>
 					<a className='inline-flex items-center p-2 mr-4 '>
 					
@@ -22,6 +22,25 @@ export const Navbar = () => {
 					</a>
 				</Link>
 
+				<button className='inline-flex p-3 hover:bg-green-600 rounded text-white ml-auto hover:text-white outline-none'>
+					<svg
+						className='w-10 h-10'
+						fill='none'
+						stroke='currentColor'
+						viewBox='0 0 24 24'
+					>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							strokeWidth={2}
+							d='M4 6h16M4 12h16M4 18h16'
+						/>
+					</svg>
+				</button>
+
+				<div className='hidden w-full lg:inline-flix lg:flex-grow lg:w-auot'>
+					
+				</div>
 			</nav>	
 		</>
 	)
