f8fae42a1b219276f95825b86044b84b2ded5c06
alexbraatz <alexjbraatz@gmail.com>
2021-06-29 12:24:41 -0500

    can live with this logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,17 +7,18 @@ export const Navbar = () => {
 			<nav className='flex items center flex-wrap bg-green-300 p-3 '>
 				<Link href='/'>
 					<a className='inline-flex items-center p-2 mr-4 '>
-						<span className='text-xl text-white font-bold uppercase tracking-wide'>
-							Alexx Braatz
+					
 								<svg
-									viewBox='0 0 200 200'
+									viewBox='0 0 24 24'
 									xmlns='http://www.w3.org/2000/svg'
-									className='h-22 w-22 mr-0'
+									className='h-12 w-12 mr-0'
 								>
 									<Logo />
 								</svg>
-								
-						</span>
+								<span className='text-xl text-white font-bold lowercase tracking-wide'>
+									Alexx Braatz
+								</span>
+							
 					</a>
 				</Link>
 
