bff549b8af1bb2d34d5e2e06cde03e1ef5833ec2
alexbraatz <alexjbraatz@gmail.com>
2021-07-20 14:21:17 -0500

    right icons are here

--- components/Contact.js
+++ components/Contact.js
@@ -12,10 +12,10 @@ export const Contact = () => {
       <h2 className="pl-7 font-sans font-extrabold text-4xl md:text-7xl ubpixel-antialiased text-black">
         contact
       </h2>
-      
-      <p><i class="fas fa-thumbs-up fa-5x"></i></p>
-        <i class="fa-solid fa-check"></i>
-        <i class="fa-github-square fa-brands"></i>
+
+        <i className="fas fa-thumbs-up fa-5x"></i>
+        <i class="fab fa-linkedin fa-5x"></i>
+        <i class="fab fa-github fa-5x"></i>
       
 
     </section>
