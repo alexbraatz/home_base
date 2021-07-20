e25d5b96f17304678b307753ff45172ca4d5e2da
alexbraatz <alexjbraatz@gmail.com>
2021-07-20 13:57:38 -0500

    icons now playing nicely with next

--- components/Contact.js
+++ components/Contact.js
@@ -1,21 +1,23 @@
+import Script from 'next/experimental-script'
 
 export const Contact = () => {
 
   return (
     <section className="pt-24 w-full h-screen bg-gradient-to-r from-pink-600 to-purple-500">
       
-      <head>
-        <script src="https://kit.fontawesome.com/d15b84a4d3.js" crossorigin="anonymous"></script>
-      </head>
+      <>
+        <Script src="https://kit.fontawesome.com/d15b84a4d3.js" crossorigin="anonymous"></Script>
+      </>
 
-      <h2 className="pl-7 font-sans font-extrabold text-4xl md:text-7xl ubpixel-antialiased text-black">contact</h2>
+      <h2 className="pl-7 font-sans font-extrabold text-4xl md:text-7xl ubpixel-antialiased text-black">
+        contact
+      </h2>
       
-        <i class="fas fa-thumbs-up fa-5x"></i><br />
+      <p><i class="fas fa-thumbs-up fa-5x"></i></p>
         <i class="fa-solid fa-check"></i>
         <i class="fa-github-square fa-brands"></i>
       
 
     </section>
-
   )
 }
